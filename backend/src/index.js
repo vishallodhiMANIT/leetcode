708cd466a91b3c852ea5b3d8ccb0f73575c9e195
vishallodhiMANIT <vishallodhi896@gmail.com>
2025-07-26 11:42:51 +0530

    Update index.js
    changes in cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const path = require("path");
 // console.log("Hello")
 const _dirname = path.resolve(); 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true 
 }))
 
