b0f8f369116acdf4adee9791f73bc60b64e1aa67
vishallodhiMANIT <vishallodhi896@gmail.com>
2025-07-30 13:28:56 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb+srv://vishal:lodhi04@cluster0.b9osdwu.mongodb.net/Leetcode')
+    await mongoose.connect('mongodb+srv://vishal:vishalBhai@cluster0.8wbrdzd.mongodb.net/Leetocde')
 }
 
 module.exports = main;
