915fc4e267ea1fb3619db17fc1c79ddeae238512
vishallodhiMANIT <vishallodhi896@gmail.com>
2025-07-31 18:15:28 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb+srv://drstrange12444440:PjKpmrqJguZBQ6v4@drstrange.icpyxcl.mongodb.net/Leetcode')
+    await mongoose.connect('mongodb+srv://drstrange12444440:PjKpmrqJguZBQ6v4@drstrange.icpyxcl.mongodb.net/?retryWrites=true&w=majority&appName=Drstrange/Leetcode')
 }
 
 module.exports = main;
